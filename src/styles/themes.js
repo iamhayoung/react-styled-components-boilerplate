c5fe37e99d590c27f2537d26bd07524696aedb15
Hayoung Gam <hahhyoung@gmail.com>
2021-07-25 04:48:58 +0900

    Add: create themes.js

---
+++ src/styles/themes.js
@@ -0,0 +1,21 @@
+const colors = {
+  white: '#fff',
+  black: '#000',
+};
+
+const fontSizes = {
+  xxlarge: '30px',
+  xlarge: '24px',
+  large: '18px',
+  medium: '15px',
+  small: '13px',
+  xsmall: '12px',
+  xxsmall: '10px',
+};
+
+const theme = {
+  colors,
+  fontSizes,
+};
+
+export default theme;
